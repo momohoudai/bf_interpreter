9d5c25078578fa4784eb5b580dee3e8ac0e92a43
Gerald Wong <email@geraldwong.net>
2022-11-23 17:19:45 +0800

    updated

--- main.c
+++ main.c
@@ -8,15 +8,28 @@
 // The code is written to as readable as possible by me. I also chose to write in C
 // so that I can fully understand what kind of parsers makes sense for a compiler.
 //
-
+// If you want to use this for whatever reason, do note that:
+// - The lexer and parser does 2 passes; once to count the amount of objects to allocate
+// and once more to actually do what it's supposed to do. This is a performance hit
+// to avoid the use of some form of realloc.
+//
+// TODO: 
+// - Dynamic data size?
+// - Error checking when out of data bounds.
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 
-#define bf_arrcnt(arr) (sizeof(arr)/sizeof(*arr))
+#define BF_PROFILE 0
 
-static const char* error = 0;
+
+#if BF_PROFILE
+# include <time.h>
+#endif 
+
+static const char* bf_error = 0;
 
 typedef enum {
   BF_TOKEN_TYPE_ADD,            // +
@@ -27,33 +40,32 @@ typedef enum {
   BF_TOKEN_TYPE_WRITE,          // .
   BF_TOKEN_TYPE_BEGIN_LOOP,     // [
   BF_TOKEN_TYPE_END_LOOP,       // ]
-} BrainFuck_Token_Type;
+} BF_Token_Type;
 
-typedef struct BrainFuck_Token {
-  BrainFuck_Token_Type type;
+typedef struct BF_Token {
+  BF_Token_Type type;
   int line;
   int at;
-} BrainFuck_Token;
+} BF_Token;
 
-typedef struct BrainFuck_Lexer {
-  int token_cap;
+typedef struct BF_Lexer {
   int token_count;
-  BrainFuck_Token* tokens;
-} BrainFuck_Lexer; 
+  BF_Token* tokens;
+} BF_Lexer; 
 
 
-typedef struct BrainFuck_Tokens {
+typedef struct BF_Tokens {
   int cap;
   int count;
-  BrainFuck_Token* e;
-} BrainFuck_Tokens; 
+  BF_Token* e;
+} BF_Tokens; 
 
 // The parser's job is to consume the tokens into a set of 'nodes' which
 // are instructions for the intepreter to execute. These nodes form the
 // Abstract Syntax Tree.
 //
 // ...Although for BrainFuck, it's just a linked list.
-typedef enum BrainFuck_Node_Type {
+typedef enum BF_Node_Type {
   BF_NODE_TYPE_LOOP,
   BF_NODE_TYPE_ADD,
   BF_NODE_TYPE_SUB,
@@ -61,86 +73,74 @@ typedef enum BrainFuck_Node_Type {
   BF_NODE_TYPE_SHIFT_RIGHT,
   BF_NODE_TYPE_READ,
   BF_NODE_TYPE_WRITE,
-} BrainFuck_Node_Type;
+} BF_Node_Type;
 
-typedef struct BrainFuck_AST {
-  struct BrainFuck_Node* first;
-  struct BrainFuck_Node* last;
-} BrainFuck_AST;
+typedef struct BF_AST {
+  struct BF_Node* first;
+  struct BF_Node* last;
+} BF_AST;
 
 
 
-typedef struct BrainFuck_AST_Stack {
+typedef struct BF_AST_Stack {
   int cap;
   int count;
-  BrainFuck_AST* e;
-} BrainFuck_AST_Stack;
+  BF_AST* e;
+} BF_AST_Stack;
 
-typedef struct BrainFuck_Node_Loop {
-  BrainFuck_AST ast;
-} BrainFuck_Node_Loop;
+typedef struct BF_Node_Loop {
+  BF_AST ast;
+} BF_Node_Loop;
 
-typedef struct BrainFuck_Node {
-  BrainFuck_Node_Type type;
-  struct BrainFuck_Node* next;
+typedef struct BF_Node {
+  BF_Node_Type type;
+  struct BF_Node* next;
   union {
-    BrainFuck_Node_Loop loop;
+    BF_Node_Loop loop;
     // others?
   };
-} BrainFuck_Node;
+} BF_Node;
 
-typedef struct BrainFuck_Parser {
+typedef struct BF_Parser {
 
-  BrainFuck_AST root_ast;
+  BF_AST root_ast;
 
-  BrainFuck_Node* nodes;
+  BF_Node* nodes;
   int node_count;
-  int node_cap;
-} BrainFuck_Parser;
+} BF_Parser;
 
 
-typedef struct BrainFuck_Allocator {
-  int node_count;
-  BrainFuck_Node nodes[256];
-
-} BrainFuck_Allocator;
-
-typedef struct BrainFuck_State {
+typedef struct BF_State {
   char data[2000];
   int at;
-} BrainFuck_State;
+} BF_State;
 
 ///////////////////////////////////////////////////////////////////////
 //
 static void
-bf_ast_stack_free(BrainFuck_AST_Stack* p) {
+bf_ast_stack_free(BF_AST_Stack* p) {
   free(p->e);
   p->count = p->cap = 0;
   p->e = 0;
 }
 
-static BrainFuck_AST*
-bf_ast_stack_push(BrainFuck_AST_Stack* p) {
-  const int grow = 10;
-  if (p->count >= p->cap) {
-    void* mem = realloc(p->e, sizeof(*p->e) * (p->cap += grow));
-    if (!mem) return 0;
-    p->e = (BrainFuck_AST*)mem;
-  }
-  BrainFuck_AST* ret = p->e + p->count++;
+static BF_AST*
+bf_ast_stack_push(BF_AST_Stack* p) {
+  BF_AST* ret = p->e + p->count++;
   ret->first = ret->last = 0;
+
   return ret ;
 }
 
 static void
-bf_ast_stack_pop(BrainFuck_AST_Stack* p) {
+bf_ast_stack_pop(BF_AST_Stack* p) {
   if (p->count > 0){
     --p->count;
   }
 }
 
-static BrainFuck_AST*
-bf_ast_stack_last(BrainFuck_AST_Stack* p) {
+static BF_AST*
+bf_ast_stack_last(BF_AST_Stack* p) {
   if (p->count > 0) {
     return p->e + p->count - 1;
   }
@@ -148,32 +148,22 @@ bf_ast_stack_last(BrainFuck_AST_Stack* p) {
 }
 
 static void
-bf_parser_free(BrainFuck_Parser* p) {
+bf_parser_free(BF_Parser* p) {
   free(p->nodes);
   p->node_count = 0;
-  p->node_cap = 0;
   p->nodes = 0;
 }
 
 
-static BrainFuck_Node*
-bf_parser_new_node(BrainFuck_Parser* p) {
-  assert(p->node_count < p->node_cap);
-  BrainFuck_Node* ret = p->nodes + p->node_count++;
+static BF_Node*
+bf_parser_new_node(BF_Parser* p) {
+  BF_Node* ret = p->nodes + p->node_count++;
   return ret;
-}
-
-static void
-bf_parser_init_nodes(BrainFuck_Parser* p, int amount) {
-  p->nodes = (BrainFuck_Node*)malloc(sizeof(*p->nodes)*amount);
-  p->node_cap = amount;
-  p->node_count = 0;
 }
 
 
 static void 
-bf_ast_push_node(BrainFuck_AST* p, BrainFuck_Node* node) {
-
+bf_ast_push_node(BF_AST* p, BF_Node* node) {
   if(!p->first) {
     p->first = p->last = node;
   }
@@ -185,7 +175,7 @@ bf_ast_push_node(BrainFuck_AST* p, BrainFuck_Node* node) {
 }
 
 static void
-bf_interpret_node(BrainFuck_State* state, BrainFuck_Node* node) {
+bf_interpret_node(BF_State* state, BF_Node* node) {
   //printf("at: %d\n", state->at);
   // TODO error handling
   //
@@ -218,7 +208,7 @@ bf_interpret_node(BrainFuck_State* state, BrainFuck_Node* node) {
       //printf("[\n");
       // Oh boy
       while(state->data[state->at] != 0) {
-        for(BrainFuck_Node* itr = node->loop.ast.first;
+        for(BF_Node* itr = node->loop.ast.first;
             itr != 0;
             itr = itr->next)
         {
@@ -233,79 +223,92 @@ bf_interpret_node(BrainFuck_State* state, BrainFuck_Node* node) {
 
 
 static void 
-bf_interpret(BrainFuck_Parser* parser) {
-  BrainFuck_AST* ast = &parser->root_ast; 
-  BrainFuck_State state = {0};
+bf_interpret(BF_Parser* parser) {
+  BF_AST* ast = &parser->root_ast; 
+  BF_State state = {0};
 
-  for(BrainFuck_Node* itr = ast->first; 
+  for(BF_Node* itr = ast->first; 
       itr != 0; 
       itr = itr->next) 
   {
     bf_interpret_node(&state, itr);
   }
 }
-
-static int 
-bf_parser_shrink_to_fit_nodes(BrainFuck_Parser* parser) {  
-  void* mem = realloc(parser->nodes, sizeof(*parser->nodes) * (parser->node_count));
-  if (!mem) return 0;
-  parser->nodes = (BrainFuck_Node*)mem;
-  parser->node_cap = parser->node_count;
-}
-
-
 static int
-bf_parser_parse(BrainFuck_Parser* parser, BrainFuck_Token* tokens, int token_count)  
+bf_parser_parse(BF_Parser* parser, BF_Token* tokens, int token_count)  
 {
-  bf_parser_init_nodes(parser, token_count);
-  BrainFuck_AST_Stack stack = {0};
-  BrainFuck_AST* cur_ast = bf_ast_stack_push(&stack);
+  // expected node and AST count
+  unsigned total_nodes = 0, total_asts = 0;
+  for(int i = 0; i < token_count; ++i) {  
+    // Basically, all tokens are a node on its own.
+    // Except for [ and ], which is considered 1 node by itself
+    if (tokens[i].type != BF_TOKEN_TYPE_END_LOOP) {
+      ++total_nodes;
+    }
+  }
+  total_asts = token_count - total_nodes;
+
+#if 0 
+  printf("total tokens: %d\n", token_count);
+  printf("total nodes: %d\n", total_nodes);
+  printf("total asts: %d\n", total_asts);
+#endif
+
+  parser->nodes = (BF_Node*)malloc(sizeof(BF_Node)*total_nodes);
+  parser->node_count = 0;
+  
+
+  BF_AST_Stack stack = {0};
+  stack.e = (BF_AST*)malloc(sizeof(BF_AST)*total_asts);
+  stack.count = 0;
+
+  BF_AST* cur_ast = bf_ast_stack_push(&stack);
 
 
   for (int token_index = 0; 
        token_index < token_count; 
        ++token_index) 
   {
-    BrainFuck_Token* token = tokens + token_index;
+    BF_Token* token = tokens + token_index;
     switch(token->type) {
       case BF_TOKEN_TYPE_ADD: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_ADD;
         bf_ast_push_node(cur_ast, node); 
       } break;
       case BF_TOKEN_TYPE_SUB: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_SUB;
         bf_ast_push_node(cur_ast, node); 
       } break;
       case BF_TOKEN_TYPE_SHIFT_LEFT: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_SHIFT_LEFT;
         bf_ast_push_node(cur_ast, node); 
       } break;
       case BF_TOKEN_TYPE_SHIFT_RIGHT: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_SHIFT_RIGHT;
         bf_ast_push_node(cur_ast, node); 
       } break;
       case BF_TOKEN_TYPE_READ: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_READ;
         bf_ast_push_node(cur_ast, node); 
       } break;
       case BF_TOKEN_TYPE_WRITE: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_WRITE;
         bf_ast_push_node(cur_ast, node); 
       } break;
       case BF_TOKEN_TYPE_BEGIN_LOOP: {
-        BrainFuck_Node* node = bf_parser_new_node(parser);
+        BF_Node* node = bf_parser_new_node(parser);
         node->type = BF_NODE_TYPE_LOOP;
         bf_ast_push_node(cur_ast, node);
         cur_ast = bf_ast_stack_push(&stack); 
       } break;
       case BF_TOKEN_TYPE_END_LOOP: {
-        BrainFuck_AST* loop_ast = cur_ast;
+        BF_AST* loop_ast = cur_ast;
 
         bf_ast_stack_pop(&stack);
         cur_ast = bf_ast_stack_last(&stack);
@@ -328,39 +331,40 @@ bf_parser_parse(BrainFuck_Parser* parser, BrainFuck_Token* tokens, int token_cou
   return 1;
 }
 
+static unsigned
+bf_lexer_count_valid_tokens(BF_Lexer* lexer, const char* src, int src_size) {
+  const char valid_tokens[] = {'+','-','.',',','<','>','[',']'};
+  int ret = 0;
+  for(int src_index = 0; src_index < src_size; ++src_index) {
+    for(int token_index = 0; token_index < sizeof(valid_tokens); ++token_index){
+      if(valid_tokens[token_index] == src[src_index]) {
+        ++ret;
+        break;
+      }
+    }
+  }
+  return ret;
+}
+
 static void
-bf_lexer_free(BrainFuck_Lexer* lexer) {
+bf_lexer_free(BF_Lexer* lexer) {
   free(lexer->tokens);
   lexer->tokens = 0;
   lexer->token_count = 0;
-  lexer->token_cap = 0;
 }
 
 static int 
-bf_lexer_push_token(BrainFuck_Lexer* lexer, BrainFuck_Token_Type type, int at, int line) {
-  const int grow = 100;
-  if (lexer->token_count >= lexer->token_cap) {
-    void* mem = realloc(lexer->tokens, sizeof(*lexer->tokens) * (lexer->token_cap += grow));
-    if (!mem) return 0;
-    lexer->tokens = (BrainFuck_Token*)mem;
-  }
-  BrainFuck_Token* token = lexer->tokens + lexer->token_count++;
+bf_set_token(BF_Token* token, BF_Token_Type type, int at, int line) {
   token->type = type;
   token->at = at;
   token->line = line;
   return 1;
 }
 
-static int 
-bf_lexer_shrink_to_fit_tokens(BrainFuck_Lexer* lexer) {  
-  void* mem = realloc(lexer->tokens, sizeof(*lexer->tokens) * (lexer->token_count));
-  if (!mem) return 0;
-  lexer->tokens = (BrainFuck_Token*)mem;
-  lexer->token_cap = lexer->token_count;
-}
+
 
 static void
-bf_print_token(BrainFuck_Token token) {
+bf_print_token(BF_Token token) {
 #define set_token_case(type) case type: printf(#type); break
   switch(token.type) 
   {
@@ -378,7 +382,7 @@ bf_print_token(BrainFuck_Token token) {
 
 #if 0
 static void 
-bf_print_all_tokens(BrainFuck_Tokens* e) {
+bf_print_all_tokens(BF_Tokens* e) {
   for(int i = 0; i < e->count; ++i) {
     printf("[");
     bf_print_token(e->e[i]);
@@ -389,34 +393,44 @@ bf_print_all_tokens(BrainFuck_Tokens* e) {
 
 
 static int 
-bf_lexer_lex(BrainFuck_Lexer* lexer, const char* src, int src_size) 
+bf_lexer_lex(BF_Lexer* lexer, const char* src, int src_size) 
 {
+  unsigned total_tokens = bf_lexer_count_valid_tokens(lexer, src, src_size); 
+  BF_Token* tokens = (BF_Token*)malloc(sizeof(*lexer->tokens)*total_tokens);
+  if (!tokens) return 0;
+
+#if BF_PROFILE
+  clock_t before = clock();
+#endif
+
   int line = 1;
+  int token_count = 0;
   for(int i = 0; i < src_size; ++i) {
+
     switch(src[i]) {
       case '+':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_ADD, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_ADD, i, line);
         break;
       case '-':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_SUB, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_SUB, i, line);
         break;
       case '<':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_SHIFT_LEFT, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_SHIFT_LEFT, i, line);
         break;
       case '>':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_SHIFT_RIGHT, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_SHIFT_RIGHT, i, line);
         break;
       case ',':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_READ, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_READ, i, line);
         break;
       case '.':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_WRITE, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_WRITE, i, line);
         break;
       case '[':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_BEGIN_LOOP, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_BEGIN_LOOP, i, line);
         break;
       case ']':
-        bf_lexer_push_token(lexer, BF_TOKEN_TYPE_END_LOOP, i, line);
+        bf_set_token(tokens + token_count++, BF_TOKEN_TYPE_END_LOOP, i, line);
         break;
       case '\n':
         ++line;
@@ -431,10 +445,15 @@ bf_lexer_lex(BrainFuck_Lexer* lexer, const char* src, int src_size)
         return 0;
     }
   }
-  if (!bf_lexer_shrink_to_fit_tokens(lexer)) {
-    bf_lexer_free(lexer);
-    return 0;
-  }
+
+#if BF_PROFILE
+  clock_t diff = clock() - before;
+  printf("Time taken %f\n", (float)diff/CLOCKS_PER_SEC);
+#endif
+
+
+  lexer->tokens = tokens;
+  lexer->token_count = token_count;
   //bf_print_all_tokens(lexer);
   return 1;
 }
@@ -442,11 +461,13 @@ bf_lexer_lex(BrainFuck_Lexer* lexer, const char* src, int src_size)
 
 static void
 bf_execute(const char* src, int src_size) {
-  BrainFuck_Lexer lexer = {0};
-  BrainFuck_Parser parser = {0};
+
+  BF_Lexer lexer = {0};
+  BF_Parser parser = {0};
 
   if (bf_lexer_lex(&lexer, src, src_size)) 
   {
+
     if (bf_parser_parse(&parser, lexer.tokens, lexer.token_count))
     {
       bf_interpret(&parser);
@@ -460,6 +481,7 @@ bf_execute(const char* src, int src_size) {
   else {
     printf("Lex error!\n");
   }
+
 }
 
 
@@ -501,8 +523,8 @@ run_file(const char* filename) {
 int main(int argc, char** argv) {
   if (argc == 2) {
     run_file(argv[1]);
-    if (error != 0) {
-      printf("\n[ERROR] %s\n", error);
+    if (bf_error != 0) {
+      printf("\n[ERROR] %s\n", bf_error);
     }
   }
   else {
